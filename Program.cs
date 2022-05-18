44630fbd378299273c63580b3446f7f95d5a2572
Menchkok <Klaskizuppi@gmail.com>
2022-05-18 20:28:44 +0700

    Complite

--- Program.cs
+++ Program.cs
@@ -12,41 +12,34 @@ namespace newtr
         {
             Console.WriteLine("hello, what is the height of the triangle ! ");
             var height = int.Parse(Console.ReadLine());
-            var triangle1 = GetTriangle(height, true);
-            var triangle2 = GetTriangle(height, false);
-
+            var triangle1 = GetTriangle(height);
             Console.WriteLine(triangle1);
-            Console.WriteLine(triangle2);
         }
-
-        static string GetTriangle(int height, bool isHalf)
+        static string GetTriangle(int height)
         {
             var result = "";
             for (int i = 0; i < height; i++)
             {
-                
-
-                if (isHalf)
+                for (int d = height; d > i; d--)
                 {
-                    
-
-                    for (int j = 0; j <= i; j++)
-                    {
-                        result += "*";
-                    }
+                    Console.Write(" ");
                 }
 
-                Console.SetCursorPosition(height - i, i + 1);
-                for (int j = 0; j <= i * 2; j++)
+                for (int j = 0; j < i; j++)
                 {
-                    Console.Write("*");
+                    Console.Write("+");
+                }
+                //УРА
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("+");
+                }
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write(" ");
                 }
                 Console.WriteLine();
-
-
-                result += "\n";
             }
-
             return result;
         }
     }
